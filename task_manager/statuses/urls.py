f2860bff443884030ee8b76551192ac5c877ab13
Gajijka <galimovgadjimurad2020@gmail.com>
2024-06-13 00:09:52 +0300

    added statuses

--- task_manager/statuses/urls.py
+++ task_manager/statuses/urls.py
@@ -3,7 +3,7 @@ from .views import CreateStatusView, ListStatusView, UpdateStatusView, DeleteSta
 
 
 urlpatterns = [
-    path('', ListStatusView.as_view(), name='list_status')
+    path('', ListStatusView.as_view(), name='list_status'),
     path('create/', CreateStatusView.as_view(), name='create_status'),
     path('<int:pk>/update', UpdateStatusView.as_view(), name='update_status'),
     path('<int:pk>/delete', DeleteStatusView.as_view(), name='delete_status')
--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -1,4 +1,31 @@
 from django.shortcuts import render
+from django.views.generic import ListView, CreateView, DeleteView, UpdateView
+from .models import Status
+from .forms import StatusForm
+from django.urls import reverse_lazy
 
 # Create your views here.
-class 
+class ListStatusView(ListView):
+    template_name = 'statuses/index.html'
+    model = Status
+    context_object_name = 'statuses'
+
+class CreateStatusView(CreateView):
+    model = Status
+    form_class = StatusForm
+    success_url = reverse_lazy('list_status')
+    success_message = 'Status was created'
+    extra_context = {'header': 'Create Status'}
+
+class UpdateStatusView(UpdateView):
+    model = Status
+    form_class = StatusForm
+    success_url = reverse_lazy('list_status')
+    success_message = 'Status was updated'
+    extra_context = {'header': 'Update status'}
+
+class DeleteStatusView(DeleteView):
+    model = Status
+    success_url = reverse_lazy('list_status')
+    success_message = 'Status was deleted'
+    extra_content = {'header': 'Delete status'}
