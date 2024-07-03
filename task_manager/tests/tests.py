67a8e57fdd2c92b9d69356cabef753a4b35d8a01
Gajijka <galimovgadjimurad2020@gmail.com>
2024-07-03 19:56:14 +0300

    Improving Tests

--- task_manager/tests/tests.py
+++ task_manager/tests/tests.py
@@ -1,27 +1,26 @@
-from django.test import TestCase, Client
+from django.test import TestCase
 from task_manager.users.models import User
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 
 
 class UserTestCase(TestCase):
-    fixtures = ['users.json',]
 
     def setUp(self):
         self.new_user = {
-                         'first_name': 'Ivan',
-                         'last_name': 'Ivanov',
-                         'username': 'IvIv',
-                         'password1': '19gmr72lp24',
-                         'password2': '19gmr72lp24',
-                        }
-        self.update_user = {
                             'first_name': 'Kirill',
                             'last_name': 'Ushakov',
                             'username': 'KiUs',
                             'password1': '19gmr72lp24',
                             'password2': '19gmr72lp24',
                            }
+        User.objects.create(
+                            id=110,
+                            first_name='Ivan',
+                            last_name='Ivanov',
+                            username='IvIv',
+                            password='19gmr72lp24'
+                           )
 
     def test_user_create(self):
         response = self.client.get(reverse('user_create'))
@@ -36,27 +35,23 @@ class UserTestCase(TestCase):
         self.assertContains(response, 'User is successfully registered')
         last_user = User.objects.last()
         count_users = User.objects.count()
-        self.assertEqual(last_user.first_name, 'Ivan')
-        self.assertEqual(count_users, 1)
+        self.assertEqual(last_user.first_name, 'Kirill')
+        self.assertEqual(count_users, 2)
 
     def test_read(self):
         response = self.client.get(reverse('users'))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'Milana_Ismailova_')
+        self.assertContains(response, 'IvIv')
 
     def test_update(self):
-        self.client.force_login(get_user_model().objects.get(id=1))
-        response = self.client.get(reverse('user_update', kwargs={'id': 1}))
-        self.assertTemplateUsed(response, 'registration/form.html')
-        response = self.client.post(reverse('user_update', kwargs={'pk': 1}),
-                                    data=self.update_user, follow=True)
+        self.client.force_login(get_user_model().objects.get(id=110))
+        response = self.client.post(reverse('user_update', kwargs={'pk': 110}),
+                                    data=self.new_user, follow=True)
         self.assertRedirects(response, reverse('users'))
-        self.assertEqual(User.objects.get(pk=1).username, 'KiUs')
+        self.assertEqual(User.objects.get(pk=110).username, 'KiUs')
 
     def test_delete(self):
-        self.client.force_login(get_user_model().objects.get(id=1))
-        response = self.client.get(reverse('user_delete', kwargs={'id': 1}))
-        self.assertTemplateUsed(response, 'delete_form.html')
-        response = self.client.post(reverse('user_delete', kwargs={'pk': 1}), follow=True)
+        self.client.force_login(get_user_model().objects.get(id=110))
+        response = self.client.post(reverse('user_delete', kwargs={'pk': 110}), follow=True)
         self.assertRedirects(response, reverse('users'))
         self.assertContains(response, 'User successfully deleted')
