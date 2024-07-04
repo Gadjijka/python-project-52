9df6540a6676a36e79e91b007e2635c9ffaffb46
Gadjijka <149161418+Gadjijka@users.noreply.github.com>
2024-07-04 13:15:10 +0300

    Update urls.py

--- task_manager/tasks/urls.py
+++ task_manager/tasks/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', ListTaskView.as_view(), name='tasks'),
     path('create/', CreateTaskView.as_view(), name='create_task'),
     path('<int:pk>/', DetailTaskView.as_view(), name='detail_task'),
-    path('<int:pk>/update', UpdateTaskView.as_view(), name='update_task'),
-    path('<int:pk>/delete', DeleteTaskView.as_view(), name='delete_task')
+    path('<int:pk>/update/', UpdateTaskView.as_view(), name='update_task'),
+    path('<int:pk>/delete/', DeleteTaskView.as_view(), name='delete_task')
 ]
