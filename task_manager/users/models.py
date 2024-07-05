42dca3fec7c10cb7c2d37fae68d3d3973f5f0d24
Gadjijka <149161418+Gadjijka@users.noreply.github.com>
2024-07-06 01:35:05 +0300

    Update models.py

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -2,7 +2,6 @@ from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import AbstractUser
 
 
-# Create your models here.
 class User(AbstractUser):
     def __str__(self):
         return self.get_full_name()
