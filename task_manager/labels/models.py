b8cf061594c68897a26a932885b7f97bf88f7782
Gadjijka <149161418+Gadjijka@users.noreply.github.com>
2024-07-06 01:32:46 +0300

    Update models.py

--- task_manager/labels/models.py
+++ task_manager/labels/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
-# Create your models here.
 class Label(models.Model):
     name = models.CharField(verbose_name=_('Name'), max_length=50, unique=True)
     created_at = models.DateTimeField(verbose_name=_('Date of creation'),
