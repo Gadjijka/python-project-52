7d1e6fdb70b4f6eb4db70c5ad96b0cb8e14c63ec
Gajijka <galimovgadjimurad2020@gmail.com>
2024-07-08 21:11:02 +0300

    Adding tests files into django-project

--- task_manager/tests/test_user.py
+++ task_manager/tests/test_user.py
@@ -2,9 +2,11 @@ from django.test import TestCase
 from task_manager.users.models import User
 from django.urls import reverse
 from django.contrib.auth import get_user_model
+from django.utils.translation import gettext as _
 
 
 class UserTestCase(TestCase):
+    fixtures = ['tests/fixtures/users.json']
 
     def setUp(self):
         self.new_user = {
@@ -14,13 +16,6 @@ class UserTestCase(TestCase):
             'password1': '19gmr72lp24',
             'password2': '19gmr72lp24',
         }
-        User.objects.create(
-            id=110,
-            first_name='Ivan',
-            last_name='Ivanov',
-            username='IvIv',
-            password='19gmr72lp24'
-        )
 
     def test_user_create(self):
         response = self.client.get(reverse('user_create'))
@@ -32,29 +27,29 @@ class UserTestCase(TestCase):
                                     )
         self.assertRedirects(response, reverse('login'))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'User is successfully registered')
+        self.assertContains(response, _('User is successfully registered'))
         last_user = User.objects.last()
         count_users = User.objects.count()
         self.assertEqual(last_user.first_name, 'Kirill')
-        self.assertEqual(count_users, 2)
+        self.assertEqual(count_users, 4)
 
     def test_read(self):
         response = self.client.get(reverse('users'))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'IvIv')
+        self.assertContains(response, 'LohDlvvc')
 
     def test_update(self):
-        self.client.force_login(get_user_model().objects.get(id=110))
-        response = self.client.post(reverse('user_update', kwargs={'pk': 110}),
+        self.client.force_login(get_user_model().objects.get(id=1))
+        response = self.client.post(reverse('user_update', kwargs={'pk': 1}),
                                     data=self.new_user, follow=True)
         self.assertRedirects(response, reverse('users'))
-        self.assertEqual(User.objects.get(pk=110).username, 'KiUs')
+        self.assertEqual(User.objects.get(pk=1).username, 'KiUs')
 
     def test_delete(self):
-        self.client.force_login(get_user_model().objects.get(id=110))
+        self.client.force_login(get_user_model().objects.get(id=1))
         response = self.client.post(reverse('user_delete',
-                                    kwargs={'pk': 110}),
+                                    kwargs={'pk': 1}),
                                     follow=True
                                     )
         self.assertRedirects(response, reverse('users'))
-        self.assertContains(response, 'User successfully deleted')
+        self.assertContains(response, _('User successfully deleted'))
